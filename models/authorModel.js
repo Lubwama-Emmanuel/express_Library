e21e04a7e504c2f357c3a7b1c9d3c30ffe517f78
Rex <lubwamaemmanuel1@gmail.com>
2022-07-12 15:23:08 +0300

    Updated populatedb

--- models/authorModel.js
+++ models/authorModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    maxLength: 100
+    maxLength: 100,
   },
   familyName: {
     type: String,
     required: true,
-    maxLength: 100
+    maxLength: 100,
   },
   dateOfBirth: {
     type: Date,
@@ -17,25 +17,25 @@ const authorSchema = new mongoose.Schema({
   dateOfDate: {
     type: Date,
   },
-})
+});
 
-authorSchema.virtual('name').get(function () {
-  const fullName = this.firstName + ' ' + this.familyName
-  return fullName
-})
+authorSchema.virtual("name").get(function () {
+  const fullName = this.firstName + " " + this.familyName;
+  return fullName;
+});
 
-authorSchema.virtual('lifeSpan').get(function () {
-  let lifeSpan = ''
+authorSchema.virtual("lifeSpan").get(function () {
+  let lifeSpan = "";
   if (this.dateOfBirth) {
-    lifeSpan = Date.now() - this.dateOfBirth
+    lifeSpan = Date.now() - this.dateOfBirth;
   } else if (this.dateOfBirth && this.dateOfDate) {
-    lifeSpan = this.dateOfDate - this.dateOfBirth
+    lifeSpan = this.dateOfDate - this.dateOfBirth;
   }
-  return lifeSpan
-})
+  return lifeSpan;
+});
 
-authorSchema.virtual('url').get(function () {
-  return '/catalog/author/' + this._id
-})
+authorSchema.virtual("url").get(function () {
+  return "/catalog/author/" + this._id;
+});
 
-module.exports = mongoose.model('Author', authorSchema)
+module.exports = mongoose.model("Author", authorSchema);
